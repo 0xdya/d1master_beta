14f6d8c57e854c4fedadc6327fc6ad83128361b0
0xdya <0xdya@proton.me>
2026-03-04 00:01:30 +0100

    Refactor preloader and sidebar functionality

--- assets/main.js
+++ assets/main.js
@@ -1,43 +1,33 @@
-    // تعطيل التمرير عند بدء الـ Preloader
+
     document.documentElement.style.overflow = 'hidden';
     document.body.style.overflow = 'hidden';
-
-    // إخفاء الـ Preloader وإعادة التمرير بعد تحميل المحتوى
     window.addEventListener("load", () => {
         setTimeout(() => {
             const preloader = document.getElementById('preloader');
             preloader.style.display = 'none';
-            
-            // إعادة التمرير
             document.documentElement.style.overflow = 'auto';
-            document.body.style.overflow = 'auto';
-
-            // عرض المحتوى المخفي مع الأنيميشن عند تحميل الصفحة
+            document.body.style.overflow = 'auto';ة
             const elements = document.querySelectorAll('.animate-on-load');
             elements.forEach(element => {
                 element.style.display = 'block';
                 element.classList.add('animate__animated', 'animate__bounceInLeft');
             });
 
-            // بعد انتهاء الـ Preloader، تفعيل الأنيميشن عند التمرير
             const observer = new IntersectionObserver((entries) => {
                 entries.forEach(entry => {
                     if (entry.isIntersecting) {
                         entry.target.classList.add('animate__animated', 'animate__fadeInLeft', 'visible'); 
-                        observer.unobserve(entry.target); // إيقاف المراقبة بعد تشغيل الأنيميشن
+                        observer.unobserve(entry.target); 
                     }
                 });
             });
 
-        }, 1500); // تأخير زمني 1.5 ثانية لعرض الـ Preloader
+        }, 1500); 
     });
 
-    /*=============== DARK LIGHT THEME ===============*/ 
     const themeButton = document.getElementById('theme-button');
     const darkTheme = 'dark-theme';
     const iconTheme = 'ri-sun-fill';
-
-    // التحقق من التفضيلات السابقة للمستخدم
     const selectedTheme = localStorage.getItem('selected-theme');
     const selectedIcon = localStorage.getItem('selected-icon');
 
@@ -57,7 +47,7 @@
         localStorage.setItem('selected-icon', getCurrentIcon());
     });
 
-    /*=============== SHOW SIDEBAR ===============*/
+  
     const showSidebar = (toggleId, sidebarId, headerId, mainId) => {
       const toggle = document.getElementById(toggleId),
             sidebar = document.getElementById(sidebarId),
@@ -74,7 +64,6 @@
     };
     showSidebar('header-toggle', 'sidebar', 'header', 'main');
 
-    // التعامل مع زر الهامبرجر والأزرار الإضافية
     const toggleButton = document.getElementById('header-toggle');
     const openSidebarButton = document.getElementById('open-sidebar-btn');
     const newOpenSidebarButton = document.getElementById('open-sidebar-btn2');
